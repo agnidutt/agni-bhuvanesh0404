44e8559891007f6ed9037a106788aa23a710875f
agnidutt <60580193+agnidutt@users.noreply.github.com>
2023-04-04 23:02:56 +0530

    Update HelloWorld.js

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SimpleCounter from './SimpleCounter';
 
+
+
 export default class HelloWorld extends React.Component {
   render() {
     return (
       <div>
-        <p>Hello world, Hi!</p>
+        <p>Hello world, This is Agni!</p>
         <SimpleCounter />
       </div>
     );
